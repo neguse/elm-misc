73057336f782a726edcaf278da53b0b0fec932f0
neguse <negcee@gmail.com>
2016-09-04 18:15:39 +0900

    Implement line feeding.

--- Popopo.elm
+++ Popopo.elm
@@ -1,12 +1,13 @@
-module Main exposing (..)
+module Popopo exposing (..)
 
 import Color exposing (Color)
-import Html exposing (Html, text)
+import Html exposing (Html, text, br)
 import Html.App as Html
 import Html.Attributes exposing (style)
 import Html.Events exposing (onMouseDown)
 import String
 import Time exposing (Time, millisecond, inSeconds)
+import List
 
 
 main =
@@ -23,12 +24,12 @@ main =
 
 
 type alias Model =
-    { index : Int }
+    { charIndex : Int, lineIndex : Int, now : Time }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { index = 0 }, Cmd.none )
+    ( { charIndex = 0, lineIndex = 0, now = 0 }, Cmd.none )
 
 
 
@@ -40,14 +41,76 @@ type Msg
     | Click
 
 
+currentCharLength : Model -> Int
+currentCharLength model =
+    case (List.head (List.drop (.lineIndex model) messages)) of
+        Just a ->
+            String.length a
+
+        Nothing ->
+            0
+
+
+nextCharIndex : Model -> Int
+nextCharIndex model =
+    min (currentCharLength model) ((.charIndex model) + 1)
+
+
+endCharIndex : Model -> Int
+endCharIndex model =
+    (currentCharLength model)
+
+
+hasNextChar : Model -> Bool
+hasNextChar model =
+    (currentCharLength model) > (.charIndex model)
+
+
+nextLineIndex : Model -> Int
+nextLineIndex model =
+    min ((List.length messages) - 1) ((.lineIndex model) + 1)
+
+
+hasNextLine : Model -> Bool
+hasNextLine model =
+    ((List.length messages) - 1) > (.lineIndex model)
+
+
+nextChar : Model -> Model
+nextChar model =
+    { model | charIndex = nextCharIndex model }
+
+
+endChar : Model -> Model
+endChar model =
+    { model | charIndex = endCharIndex model }
+
+
+nextLine : Model -> Model
+nextLine model =
+    { model | charIndex = 0, lineIndex = nextLineIndex model }
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Tick newTime ->
-            ( { model | index = (min ((.index model) + 1) (String.length messageBody)) }, Cmd.none )
+            let
+                nowModel =
+                    { model | now = newTime }
+            in
+                if hasNextChar nowModel then
+                    ( (nextChar nowModel), Cmd.none )
+                else
+                    ( nowModel, Cmd.none )
 
         Click ->
-            ( { model | index = 0 }, Cmd.none )
+            if hasNextChar model then
+                ( (endChar model), Cmd.none )
+            else if hasNextLine model then
+                ( (nextLine model), Cmd.none )
+            else
+                ( model, Cmd.none )
 
 
 
@@ -63,8 +126,43 @@ subscriptions model =
 -- VIEW
 
 
-messageBody =
-    "ああああああああ"
+messages =
+    [ "寿限無\x3000寿限無"
+    , "五劫の擦り切れ"
+    , "海砂利水魚の"
+    , "水行末 雲来末 風来末"
+    , "食う寝る処に住む処"
+    , "藪ら柑子の藪柑子"
+    , "パイポ\x3000パイポ"
+    , "パイポのシューリンガン"
+    , "シューリンガンのグーリンダイ"
+    , "グーリンダイのポンポコピーのポンポコナーの"
+    , "長久命の長助"
+    ]
+
+
+viewNextLine : Model -> String
+viewNextLine model =
+    if (not (hasNextChar model)) && (hasNextLine model) && ((round (inSeconds (.now model))) % 2 == 0) then
+        "▼"
+    else
+        ""
+
+
+viewMessages : Model -> List (Html msg)
+viewMessages model =
+    (List.intersperse (br [] [])
+        (List.append
+            (List.map text (List.take (.lineIndex model) messages))
+            (case (List.head (List.drop (.lineIndex model) messages)) of
+                Just a ->
+                    [ text (String.left (.charIndex model) a ++ (viewNextLine model)) ]
+
+                Nothing ->
+                    []
+            )
+        )
+    )
 
 
 view : Model -> Html Msg
@@ -76,5 +174,4 @@ view model =
             , ( "height", "100%" )
             ]
         ]
-        [ text (String.left (.index model) messageBody)
-        ]
+        (viewMessages model)
